75321132509ea0fbcc68383063d2a37f8afa6341
Amanda Maziero <mandahmaziero1999@gmail.com>
2019-05-23 08:03:35 -0300

    tamo melhorando

--- resources/views/layout/cabecalho.blade.php
+++ resources/views/layout/cabecalho.blade.php
@@ -1,13 +1,36 @@
 <header>
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item"><a href="#">O que posso doar?</a></li>
-            <li class="breadcrumb-item"><a href="#">Instituições Parceiras</a></li>
-            <li class="breadcrumb-item"><a href="#">Contato</a></li>
-            <li class="breadcrumb-item"><a href="#">Sobre Nós</a></li>
-            <li class="breadcrumb-item"><a href="#">Login</a></li>
-        </ol>
+    <nav class="navbar navbar-light" style="background-color: #D484E8;">
+  <a class="navbar-brand mb-0 h1" href="#">Doáryo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+        <a class="nav-link" href="#">Página Inicial<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+        <a class="nav-link" href="#">O que posso doar?</a>
+        </li>
+        <li class="nav-item active">
+        <a class="nav-link" href="#">Instituições Parceiras</a>
+        </li>
+        <li class="nav-item active">
+        <a class="nav-link" href="#">Contato</a>
+        </li>
+        <li class="nav-item active">
+        <a class="nav-link" href="#">Sobre Nós</a>
+        </li>
+        <form class="form-inline">
+            <button class="btn btn-light" type="button">Entrar</button>
+        </form>
+    
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Digite aqui..." aria-label="Search">
+      <button class="btn btn-light my-2 my-sm-0" type="submit">Pesquisar</button>
+    </form>
+</div>
     </nav>
 </header>
 
