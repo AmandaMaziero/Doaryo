212d6267946443ca8ff0a84c3c95efd10db61da3
Amanda Maziero <mandahmaziero1999@gmail.com>
2019-07-25 08:04:05 -0300

    Update index.blade.php
    perfil instituição a caminho

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -6,10 +6,54 @@
 <div class="container">
     <h1 class='display-1 text-center'>Perfil</h1>
 </div>
+<br>
+<div class="container text-center">
+    <div class="list-group list-group-horizontal">
+        <button type="button" class="list-group-item list-group-item-action" href="#">Requisições</button>
+        <button type="button" class="list-group-item list-group-item-action" href="#">Editar Perfil</button>
+        <button type="button" class="list-group-item list-group-item-action" href="#">Sair da Conta</button>
+        <button type="button" class="list-group-item list-group-item-action" href="#">Excluir Conta</button>
+    </div>
+</div>
+<br><br>
 
 <div class="container text-center">
-    <h2>Veja abaixo as opções de doações:</h2>
+    <div class="row">
 
+        <div class="col-6">
+            <h2>Requisição</h2>
+            <p>teste</p>
+        </div>
 
+        <div class="col-6">
+            <h2>Quantidade</h2>
+
+            <div class="row">
+                <div class="col-6">
+                    <p>teste</p>
+                </div>
+
+                <div class="col-6">
+                    <div class="list-group list-group-horizontal">
+                        <button type="button" class="list-group-item list-group-item-action" href="#">Excluir</button>
+                        <button type="button" class="list-group-item list-group-item-action" href="#">Editar</button>
+                    </div>
+                </div> 
+            </div>
+            
+        </div>
+    </div>
 </div>
+<br><br>
+
+<div class="container text-center">
+    
+    <div class="list-group list-group-horizontal">
+        <button type="button" class="list-group-item list-group-item-action" href="#">Excluir todas</button>
+        <button type="button" class="list-group-item list-group-item-action" href="#">Adicionar Requisição</button>
+        <button type="button" class="list-group-item list-group-item-action" href="#">Requisições Pendentes</button>
+        <button type="button" class="list-group-item list-group-item-action" href="#">Doações Recebidas</button>
+    </div>
+</div>
+ 
 @endsection 
