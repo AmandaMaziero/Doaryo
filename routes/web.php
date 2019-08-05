4a543c7210d61be74f87bcbc9b8f898bdcfe49c0
rafaeldamiam <rafael.damiam23@gmail.com>
2019-08-05 18:36:59 -0300

    att routes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+//home
+Route::get('/', ["uses"=>"HomeControlador@index"]);
+Route::get('/home', 'HomeControlador@index')->name('home');
+
+
 Route::get('/cadastro', ["uses"=>"CadastroControlador@index"]);
 
 Route::get('/carrinho', ["uses"=>"CarrinhoControlador@index"]);
@@ -18,7 +24,7 @@ Route::get('/contato', ["uses"=>"ContatoControlador@index"]);
 
 Route::get('/doacao', ["uses"=>"DoacaoControlador@index"]);
 
-Route::get('/', ["uses"=>"HomeControlador@index"]);
+
 
 Route::get('/instituicoes', ["uses"=>"InstituicoesControlador@index"]);
 
@@ -29,6 +35,6 @@ Route::get('/perfil', ["uses"=>"PerfilControlador@index"]);
 Route::get('/sobrenos', ["uses"=>"SobrenosControlador@index"]);
 
 
-Auth::routes();
+//Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
